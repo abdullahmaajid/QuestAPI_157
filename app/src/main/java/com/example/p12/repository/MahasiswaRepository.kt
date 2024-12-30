e8a1c086ff43e269238753d4d49efd24ff6d22b1
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-30 08:21:16 +0700

    Membuat file untuk Repository | MahasiswaRepository.kt

---
+++ app/src/main/java/com/example/p12/repository/MahasiswaRepository.kt
@@ -0,0 +1,45 @@
+package com.example.p12.repository
+
+
+import com.example.p12.model.Mahasiswa
+import com.example.p12.service_api.MahasiswaService
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaByNim(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(private val mahasiswaService: MahasiswaService)
+    : MahasiswaRepository
+{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw Exception("Failed to delete mahasiswa. HTTP Status Code: ${response.code()}")
+            }
+            else{
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaService.getMahasiswa()
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return mahasiswaService.getMahasiswaByNim(nim)
+    }
+}
