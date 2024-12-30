d8c2b5ca279e745e5dd0eada1289fd20b6bfdfcb
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-12-30 08:22:57 +0700

    Membuat file untuk Applications | MahasiswaApplications.kt

---
+++ app/src/main/java/com/example/p12/applications/MahasiswaApplications.kt
@@ -0,0 +1,16 @@
+package com.example.p12.applications
+
+
+
+import android.app.Application
+import com.example.p12.dependenciesinjection.AppContainer
+import com.example.p12.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApplications : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
